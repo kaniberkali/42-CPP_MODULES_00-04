eb6143c4da87b9d45786d43ca933e22c9b2ba5f6
Ali Kanıberk <82172758+kaniberkali@users.noreply.github.com>
2023-12-04 16:40:19 +0300

    Add files via upload

--- cpp01/ex06/Harl.cpp
+++ cpp01/ex06/Harl.cpp
@@ -2,7 +2,7 @@
 
 Harl::Harl()
 {
-    
+	
 }
 
 Harl::~Harl()
@@ -10,56 +10,53 @@ Harl::~Harl()
 
 }
 
-void    Harl::debug(void)
+void    Harl::debug(void) const
 {
-    std::cout << "[ DEBUG ]" << std::endl;
-    std::cout << "love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl;
-    std::cout << std::endl;
+	std::cout << "[ DEBUG ]" << std::endl;
+	std::cout << "love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl;
+	std::cout << std::endl;
 }
 
-void    Harl::info(void)
+void    Harl::info(void) const
 {
-    std::cout << "[ INFO ]" << std::endl;
-    std::cout << "I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!" << std::endl;
-    std::cout << std::endl;
+	std::cout << "[ INFO ]" << std::endl;
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!" << std::endl;
+	std::cout << std::endl;
 }
 
-void    Harl::warning(void)
+void    Harl::warning(void) const
 {
-    std::cout << "[ WARNING ]" << std::endl;
-    std::cout << "I think I deserve to have some extra bacon for free. I've been coming foryears whereas you started working here since last month." << std::endl;
-    std::cout << std::endl;
+	std::cout << "[ WARNING ]" << std::endl;
+	std::cout << "I think I deserve to have some extra bacon for free." << std::endl << "I've been coming foryears whereas you started working here since last month." << std::endl;
+	std::cout << std::endl;
 }
 
-void    Harl::error(void)
+void    Harl::error(void) const
 {
-    std::cout << "[ ERROR ]" << std::endl;
-    std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
-    std::cout << std::endl;
+	std::cout << "[ ERROR ]" << std::endl;
+	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+	std::cout << std::endl;
 }
 
-void    Harl::complain(std::string level)
+void    Harl::complain(std::string level) const
 {
-    std::string levels[] = { "DEBUG", "INFO", "WARNING", "ERROR"};
-    int i;
-    i = 0;
-    while (levels[i] != level)
-        i++;
-    switch(i)
-    {
-        case    0:
-            this->debug();
-            break;
-        case    1:
-            this->info();
-            break;
-        case    2:
-            this->warning();
-            break;
-        case    3:
-            this->error();
-            break;
-        default:
-                std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
-    }   
+	std::string levels[] = { "DEBUG", "INFO", "WARNING", "ERROR"};
+	int i;
+	i = 0;
+	while (levels[i] != level && i < 4)
+		i++;
+	switch(i)
+	{
+		case    0:
+			this->debug();
+		case    1:
+			this->info();
+		case    2:
+			this->warning();
+		case    3:
+			this->error();
+			break;
+		default:
+				std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+	}   
 }
--- cpp01/ex06/Harl.hpp
+++ cpp01/ex06/Harl.hpp
@@ -6,14 +6,14 @@
 class Harl
 {
     private:
-        void    debug(void);
-        void    info(void);
-        void    warning(void);
-        void    error(void);
+        void    debug(void) const;
+        void    info(void) const;
+        void    warning(void) const;
+        void    error(void) const;
     public:
         Harl();
         ~Harl();
-        void    complain(std::string level);
+        void    complain(std::string level) const;
 };
 
 typedef void (Harl::*t_func) ( void );
